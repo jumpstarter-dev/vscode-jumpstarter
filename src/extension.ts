3f92c555c6b175d8256b8a2c661627bdc1cb7ef1
Kirk Brauer <kirkebrauer@gmail.com>
2025-03-09 22:28:44 -0400

    Add basic Jumpstarter command calling on Python env change

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { PythonExtension } from '@vscode/python-extension';
+
+const { execa } = require('execa');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext): Promise<any> {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -19,7 +22,37 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from vscode-jumpstarter!');
 	});
 
+	const python = await getPythonExtension();
+	if (python) {
+		await checkJumpstarterVersion(python);
+		const pythonEnvironment = python.environments.onDidChangeActiveEnvironmentPath((event) => {
+			console.log("Python environment changed", event);
+			checkJumpstarterVersion(python);
+		});
+		context.subscriptions.push(pythonEnvironment);
+	}
+
 	context.subscriptions.push(disposable);
+}
+
+async function checkJumpstarterVersion(python: PythonExtension): Promise<boolean> {
+	const path = python.environments.getActiveEnvironmentPath().path;
+	console.log("Found Python extension! Active environment path:", path);
+	const { stdout } = await execa`${path} -m jumpstarter_cli version --output json`;
+	console.log("Command Output: ", stdout);
+	return true;
+}
+
+/**
+ * Return the python extension's API, if available.
+ */
+async function getPythonExtension(): Promise<PythonExtension | undefined> {
+	try {
+		return await PythonExtension.api();
+	} catch (err) {
+		console.error(`Unable to load python extension: ${err}`);
+		return undefined;
+	}
 }
 
 // This method is called when your extension is deactivated
